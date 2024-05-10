9900eb25783dd6fe191c5ace81826d3df322aed1
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-10 14:08:13 -0600

    change dimen

--- lib/utils/commons/dimens_app.dart
+++ lib/utils/commons/dimens_app.dart
@@ -1,6 +1,8 @@
 const double simpleSmallSeparation = 4.0;
 const double simpleSeparation = 8.0;
 const double doubleSeparation = 16.0;
+const double doubleMediumSeparation = 24.0;
+const double doubleBigSeparation = 32.0;
 
 const double radiusSmall = 4.0;
 const double radiusMedium = 8.0;
@@ -9,4 +11,3 @@ const double radiusBig = 12.0;
 const double elevationSmall = 4.0;
 const double elevationMedium = 8.0;
 const double elevationBig = 12.0;
-
