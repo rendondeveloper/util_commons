3b2a0e8a636d11a076c4ddcf62ff5ba8137468b4
Luis Enrique Rendon Cortez <enriquerendonc@platzi.com>
2024-05-14 17:55:00 -0600

    create new functions

--- lib/utils/extensions/extension_string.dart
+++ lib/utils/extensions/extension_string.dart
@@ -1,1 +1,41 @@
-import 'package:flutter/foundation.dart';extension CustomString on String {  bool toBool() => this == "1";  void log() {    if (kDebugMode) {      print("LOG -> $this");    }  }  String toFormat(String text){    return replaceAll("%", text);  }}extension CustomStringNull on String? {  bool isNumber() {    return this != null ? double.tryParse(this!) != null : false;  }  int toInt() {    return this != null ? int.tryParse(this!) ?? -1  : -1;  }  String getDataIfNotNull() {      return this == null ? "" : this!;  }}
+import 'package:flutter/foundation.dart';
+
+extension CustomString on String {
+  bool toBool() => this == "1";
+
+  void log() {
+    if (kDebugMode) {
+      print("LOG -> $this");
+    }
+  }
+
+  String toFormat(String text) {
+    return replaceAll("%", text);
+  }
+}
+
+extension CustomStringNull on String? {
+  bool isNumber() {
+    return this != null ? double.tryParse(this!) != null : false;
+  }
+
+  int toInt() {
+    return this != null ? int.tryParse(this!) ?? -1 : -1;
+  }
+
+  String getDataIfNotNull() {
+    return this == null ? "" : this!;
+  }
+
+  void run(Function(String) invoke) {
+    if (this != null) {
+      invoke(this!);
+    }
+  }
+
+  void let(Function(String) invoke) {
+    if (this != null) {
+      invoke(this!);
+    }
+  }
+}
