76bf2d27adbcbedb9565349638ec29721a36d423
luis rendon <luis.rendon@aplazo.mx>
2024-10-30 09:39:57 -0600

    ✨ Add a new method to know the device type

--- lib/utils/extensions/extension_context.dart
+++ lib/utils/extensions/extension_context.dart
@@ -1,1 +1,14 @@
-import 'package:flutter/material.dart';extension CustomContext on BuildContext {  ThemeData get getThemeData => Theme.of(this);  MediaQueryData get getMediaQuery => MediaQuery.of(this);}
+import 'package:flutter/material.dart';
+import 'package:util_commons/utils/commons/device_type.dart';
+
+extension CustomContext on BuildContext {
+
+  ThemeData get getThemeData => Theme.of(this);
+
+  MediaQueryData get getMediaQuery => MediaQuery.of(this);
+
+  DeviceType get getDeviceType {
+    final data = getMediaQuery.size.shortestSide;
+    return data < 550 ? DeviceType.phone : DeviceType.tablet;
+  }
+}
